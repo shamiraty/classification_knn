5b3e48bb24deba3427aa9f73b94a7cf6b4a975e0
shamiraty <129072179+shamiraty@users.noreply.github.com>
2025-01-20 06:41:40 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from numerize import numerize
 
 # Streamlit app setup
 st.set_page_config(layout="wide")
-st.title("KNN ALGORITHM AND PREDICTION WITH MYSQL")
-
+st.title("K-NEIGHBORS ALGORITHM: PREDICTION & TRENDS WITH MYSQL")
+st.warning("Predicting Future Customer Behaviors Starts Here")
 # Main objective
 st.subheader("Main Objective")
 st.markdown("""
